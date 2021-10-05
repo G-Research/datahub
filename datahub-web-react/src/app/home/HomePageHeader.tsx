702ce940d9ff657ca919c66a7914d4723b20ffea
Eddie (Xiao Di) Li <xdl@users.noreply.github.com>
2021-10-05 10:51:40 +0100

    Revert "fix(react): fix manrope styling (#3311)"
    This reverts commit 453d39426482d232707039ad1041fb0d103eca92.

--- datahub-web-react/src/app/home/HomePageHeader.tsx
+++ datahub-web-react/src/app/home/HomePageHeader.tsx
@@ -31,7 +31,6 @@ const WelcomeText = styled(Typography.Text)`
 
 const SubHeaderText = styled(Typography.Text)`
     font-size: 20px;
-    font-weight: 500;
     color: ${(props) =>
         props.theme.styles['homepage-text-color'] || props.theme.styles['homepage-background-lower-fade']};
 `;
